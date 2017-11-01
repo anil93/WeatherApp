faee6ad97d69018be1a8d9aab3453d867d068761
anilaynaci <anil.aynaci@hotmail.com>
2017-11-01 14:52:25 +0300

    Preferences mekanizması kullanılarak son hava durumu bilgisinin cihazda tutulma işlemi yapıldı.

--- app/src/main/java/com/anilaynaci/weatherapp/MainActivity.java
+++ app/src/main/java/com/anilaynaci/weatherapp/MainActivity.java
@@ -2,16 +2,22 @@ package com.anilaynaci.weatherapp;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -21,7 +27,10 @@ import android.widget.Toast;
 
 import com.anilaynaci.weatherapp.entities.List;
 import com.anilaynaci.weatherapp.entities.RootObject;
+import com.google.gson.Gson;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -33,8 +42,6 @@ public class MainActivity extends AppCompatActivity {
     java.util.List<List> secondDate;
     java.util.List<List> thirdDate;
     java.util.List<List> fourthDate;
-
-    LocationManager lm;
 
     Drawable[] drawables;
 
@@ -53,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
     TextView txtFourthTempMin;
     TextView txtCurrentMain;
     TextView txtCurrentDescription;
+    TextView txtUpdate;
 
     ImageView imgCurrent;
     ImageView imgFirst;
@@ -63,6 +71,9 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout topLayout;
     LinearLayout middleLayout;
     LinearLayout bottomLayout;
+
+    SharedPreferences sp;
+    SharedPreferences.Editor edt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -104,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
         txtFourthTempMin = findViewById(R.id.txtFourthTempMin);
         txtCurrentMain = findViewById(R.id.txtCurrentMain);
         txtCurrentDescription = findViewById(R.id.txtCurrentDescription);
+        txtUpdate = findViewById(R.id.txtUpdate);
 
         imgCurrent = findViewById(R.id.imgCurrent);
         imgFirst = findViewById(R.id.imgFirst);
@@ -116,6 +128,28 @@ public class MainActivity extends AppCompatActivity {
         bottomLayout = findViewById(R.id.bottomLayout);
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        sp = this.getPreferences(Context.MODE_PRIVATE);
+        edt = sp.edit();
+
+        if (sp.contains("rootObject")) {
+            try {
+                Gson gson = new Gson();
+                String json = sp.getString("rootObject", "");
+                RootObject r = gson.fromJson(json, RootObject.class);
+                txtUpdate.setText(sp.getString("updateTime", ""));
+                drawables = new Drawable[5];
+                for(int i = 0; i < drawables.length ;i++){
+                    byte[] array = Base64.decode(sp.getString(Integer.toString(i),"null"), Base64.DEFAULT);
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(array, 0, array.length);
+                    Drawable drawable = new BitmapDrawable(getBaseContext().getResources(), bitmap);
+                    drawables[i] = drawable;
+                }
+                showInputValue(r, drawables, true);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -125,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, "GPS izni verilmedi. Konum almak için kabul etmelisin.", Toast.LENGTH_LONG).show();
             } else {
                 if (ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "GPS izni verildi.", Toast.LENGTH_LONG).show();
+                    //Toast.makeText(this, "GPS izni verildi.", Toast.LENGTH_LONG).show();
                     getGeoCoord();
                 } else {
                     Toast.makeText(this, "GPS izni verilmemiş. Tekrar sorma olarak belirtilmiş. Lütfen uygulama ayarlarını sıfırlayın.", Toast.LENGTH_LONG).show();
@@ -142,62 +176,73 @@ public class MainActivity extends AppCompatActivity {
             if (location != null) {
                 String latitude = Double.toString(location.getLatitude());
                 String longitude = Double.toString(location.getLongitude());
+                txtCity.setText(latitude + " - " + longitude);
                 new RetrieveRestClient().execute(latitude, longitude);
             } else {
-                Toast.makeText(MainActivity.this, "Konum bilgisi alınamıyor", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Konum bilgisi alınamıyor.", Toast.LENGTH_LONG).show();
             }
         }
 
         @Override
         public void onStatusChanged(String provider, int status, Bundle extras) {
-
+            //Toast.makeText(MainActivity.this, "Status changed", Toast.LENGTH_LONG).show();
         }
 
         @Override
         public void onProviderEnabled(String provider) {
+            Toast.makeText(MainActivity.this, "Uygulamaya yönlendiriliyorsunuz...", Toast.LENGTH_LONG).show();
+            new Handler().postDelayed(new Runnable() {
 
+                @Override
+                public void run() {
+                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            }, 3000);
         }
 
         @Override
         public void onProviderDisabled(String provider) {
-
-        }
-    }
-
-    public boolean isGPSEnabled() {
-
-        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        if (!lm.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            return false;
-        } else {
-            return true;
+            //Toast.makeText(MainActivity.this, "Disabled", Toast.LENGTH_LONG).show();
         }
     }
 
     public void getGeoCoord() {
 
-        if (!isGPSEnabled()) {
-            txtCity.setText("GPS kapalı! Konum verisi alınamıyor.");
-        } else {
-            txtCity.setText("Konum verisi alınıyor...");
-        }
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
         GpsReceiver receiver = new GpsReceiver();
-        lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-        Criteria crt = new Criteria();
-        crt.setAccuracy(Criteria.ACCURACY_FINE);
-        crt.setPowerRequirement(Criteria.POWER_MEDIUM);
-        crt.setSpeedRequired(false);
-        crt.setAltitudeRequired(false);
-        String provider = lm.getBestProvider(crt, true);
+        //3 saat
+        Long minTime = 3 * (60 * 60) * 1000L;
+        //5 km
+        float minDistance = 5000.f;
+
+        boolean isNetworkEnabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        if (!isNetworkEnabled) {
+
+            txtCity.setText("GPS Kapalı! Ayarlara yönlendiriliyorsunuz...");
+
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    //Toast.makeText(MainActivity.this, "hello world", Toast.LENGTH_SHORT).show();
+
+                    Intent viewIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(viewIntent);
+                }
+            }, 3000);
+        } else {
+            txtCity.setText("Veriler alınıyor...");
+        }
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
 
-        lm.requestLocationUpdates(provider, 9999999L, 9999999.0F, receiver);
+        lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, minTime, minDistance, receiver);
     }
 
     class RetrieveRestClient extends AsyncTask<String, Void, RootObject> {
@@ -215,6 +260,9 @@ public class MainActivity extends AppCompatActivity {
                 //bugünün iconu
                 String currentImageCode = currentDate.get(0).getWeather().get(0).getIcon() + ".png";
                 Drawable currentImageIcon = restClient.getDrawable(currentImageCode);
+                /*byte[] arr = restClient.getImage(currentImageCode);
+                Bitmap bmp = BitmapFactory.decodeByteArray(arr, 0, arr.length);
+                Drawable currentImageIcon = new BitmapDrawable(getBaseContext().getResources(), bmp);*/
                 drawables[0] = currentImageIcon;
 
                 //birinci günün iconu
@@ -247,12 +295,32 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(RootObject r) {
 
             if (r != null) {
-                showInputValue(r, drawables);
+                showInputValue(r, drawables, false);
+            }
+
+            //Son güncelleme tarihi
+            Calendar c = Calendar.getInstance();
+            SimpleDateFormat date = new SimpleDateFormat("yyyy-MM-dd");
+            String currentDate = date.format(c.getTime());
+            SimpleDateFormat time = new SimpleDateFormat("HH:mm");
+            String currentTime = time.format(c.getTime());
+            String updateTime = currentDate + " " + currentTime;
+
+            try {
+                Gson gson = new Gson();
+                //edt = sp.edit();
+                String json = gson.toJson(r);
+                edt.putString("rootObject", json);
+                edt.putString("updateTime", updateTime);
+                edt.commit();
+                txtUpdate.setText(sp.getString("updateTime", ""));
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
 
-    private void showInputValue(RootObject r, Drawable[] d) {
+    private void showInputValue(RootObject r, Drawable[] d, boolean flag) {
 
         if (r.getCity() != null) {
             String cityName = r.getCity().getName();
@@ -262,7 +330,10 @@ public class MainActivity extends AppCompatActivity {
             txtCity.setText("Şehir bilgisi alınamadı.");
         }
 
-        imgCurrent.setImageDrawable(d[0]);
+        if (flag) {
+            getWeatherByDate(r);
+        }
+
         int tempMin = (int) (Math.round(currentDate.get(0).getMain().getTemp()) - 273d);
         txtCurrentTemp.setText(tempMin + "°C");
         txtCurrentMain.setText(currentDate.get(0).getWeather().get(0).getMain());
@@ -278,10 +349,13 @@ public class MainActivity extends AppCompatActivity {
         String wind = windSpeed + "m/s " + DegreesToCardinalDetailed(windDegree);
         txtWind.setText(wind);
 
-        imgFirst.setImageDrawable(d[1]);
-        imgSecond.setImageDrawable(d[2]);
-        imgThird.setImageDrawable(d[3]);
-        imgFourth.setImageDrawable(d[4]);
+        if (d != null) {
+            imgCurrent.setImageDrawable(d[0]);
+            imgFirst.setImageDrawable(d[1]);
+            imgSecond.setImageDrawable(d[2]);
+            imgThird.setImageDrawable(d[3]);
+            imgFourth.setImageDrawable(d[4]);
+        }
 
         txtFirstTempMax.setText(compareMaxTemp(firstDate) + "°");
         txtFirstTempMin.setText(compareMinTemp(firstDate) + "°");
@@ -297,6 +371,19 @@ public class MainActivity extends AppCompatActivity {
 
         middleLayout.setVisibility(View.VISIBLE);
         bottomLayout.setVisibility(View.VISIBLE);
+
+        if (!flag) {
+            for (int i = 0; i < d.length; i++) {
+                Bitmap bitmap = ((BitmapDrawable) d[i]).getBitmap();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] bitmapdata = stream.toByteArray();
+                String saveThis = Base64.encodeToString(bitmapdata, Base64.DEFAULT);
+                //edt = sp.edit();
+                edt.putString(Integer.toString(i), saveThis);
+            }
+            //edt.commit();
+        }
     }
 
     private int compareMaxTemp(java.util.List<List> firstDateMain) {
